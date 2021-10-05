2ada2675ed5fef093a7c090bff10ba811bcd2d12
Kidays <shenxianliang@live.cn>
2021-10-05 23:18:21 +0800

    fuel filling question

---
+++ Algorithm/Greedy.py
@@ -0,0 +1,17 @@
+def oil(n, distance):
+    i = 0
+    count = 0
+    for one in distance:
+        count += one
+        if count > n:
+            print('%dkm start fuel filling' % (one))
+            count = one
+            i += 1
+    return i
+
+
+n = 300
+dis = [150, 180, 120, 100, 280, 160, 50, 60, 20, 140, 130]  # dis<300
+num = oil(n, dis)
+print('the minimum filling times:%d' % (num))
+# i=0 n=300 count=300 count=120 i=2 one=180
